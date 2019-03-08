46b2d2103a2eaa358b38d9da7048fcef67e350f8
Thog <me@thog.eu>
2019-03-08 18:37:25 +0100

    Separate fuc5 from the rest & implement decoding of some instructions
    - mov 0xD0 is implemented for fuc5
    - 0xCx & 0xDx instructions are now decoded

--- data/testing/faucon.asm
+++ data/testing/faucon.asm
@@ -1,6 +1,8 @@
 mov $r13 0xf;
 mov $r13 0xff;
 mov $r13 0xffff;
+mov $r13 0xffffffff;
+
 mulu $r1 $r13 0x10;
 muls $r1 $r13 0x10;
 sext $r1 $r13 0x10;
@@ -8,6 +10,24 @@ and $r1 $r13 0x10;
 or $r1 $r13 0x10;
 xor $r1 $r13 0x10;
 xbit $r1 $r13 0x10;
+// TODO: ce
 iord $r13 I[$r13 + 0x4];
-iowr I[$r9] $r15;
+
+mulu $r1 $r13 0x1010;
+muls $r1 $r13 0x1010;
+and $r1 $r13 0x1010;
+or $r1 $r13 0x1010;
+xor $r1 $r13 0x1010;
+
+mulu $r1 0x10;
+muls $r1 0x10;
+sext $r1 0x10;
+// TODO: sethi
+and $r1 0x10;
+or $r9 0x10;
+xor $r9 0x10;
+bset $r9 0x10;
+bclr $r9 0x10;
+btgl $r9 0x10;
+//xbit $r9 $flags z
 exit;
